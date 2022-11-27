d41bf0f1f9287e45fdbba26b0064f13e063dae92
e-mm-u <uh.habiba1012@gmail.com>
2022-11-27 23:45:33 +0600

    usetoken hook created for jwt

---
+++ src/hooks/useToken.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+const useToken = (email) => {
+    const [token, setToken] = useState(null);
+
+    useEffect(()=>{
+        if(email){
+            fetch(`http://localhost:5000/jwt?email=${email}`)
+                .then( res => res.json())
+                .then( data => {
+                    if(data.access_token){
+                        localStorage.setItem('access_token', data.access_token);
+                        setToken(data.access_token)
+                    }
+                });
+        }
+    }, [email])
+
+    return {token};
+};
+
+export default useToken;
